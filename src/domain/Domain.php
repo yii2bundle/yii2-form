294ba6877f8ddb05a8d4d7b17af9a46491c0decd
Vitaliy <theyamshikov@yandex.ru>
2019-06-11 20:01:05 +0600

    init

---
+++ src/domain/Domain.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace yii2bundle\form\domain;
+
+use yii2rails\app\domain\helpers\EnvService;
+
+class Domain extends \yii2rails\domain\Domain {
+
+	public function config() {
+		return [
+			'services' => [
+
+			],
+		];
+	}
+
+}
